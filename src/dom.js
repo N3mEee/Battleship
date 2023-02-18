29d3fd26ae4d991fd8f15db0850ddd4c04ca6e61
N3mEee <n3mebusiness@gmail.com>
2023-02-18 15:46:06 +0100

    refactor dom and update the style

--- src/dom.js
+++ src/dom.js
@@ -1,6 +1,34 @@
 import "./style.css";
 
 const main = document.querySelector("main");
+
+function createBoard(board, container, isPlayer) {
+    const boardElement = document.createElement("div");
+    boardElement.classList.add("board");
+    if (!isPlayer) boardElement.classList.add("ai-board");
+    container.appendChild(boardElement);
+
+    board.forEach((row, rowIndex) => {
+        row.forEach((cell, cellIndex) => {
+            const cellElement = document.createElement("div");
+            cellElement.classList.add("cell");
+            cellElement.dataset.row = rowIndex;
+            cellElement.dataset.col = cellIndex;
+
+            if (isPlayer) {
+                if (cell === 1) cellElement.classList.add("ship");
+                else if (cell === 2) cellElement.classList.add("damaged-ship");
+                else if (cell === 3) cellElement.classList.add("missed-attack");
+            } else {
+                if (cell === 2) cellElement.classList.add("damaged-ship");
+                else if (cell === 3) cellElement.classList.add("missed-attack");
+            }
+
+            boardElement.appendChild(cellElement);
+        });
+    });
+}
+
 function drawBoard(board1, board2) {
     main.innerHTML = "";
 
@@ -8,62 +36,26 @@ function drawBoard(board1, board2) {
     boardContainer.classList.add("board-container");
     main.appendChild(boardContainer);
 
-    const boardPlayer1 = document.createElement("div");
-    boardPlayer1.classList.add("board");
-    boardContainer.appendChild(boardPlayer1);
-
-    for (let i = 0; i < board1.length; i++) {
-        for (let j = 0; j < board1[i].length; j++) {
-            const cell = document.createElement("div");
-            cell.classList.add("cell");
-            cell.dataset.row = `${i}`;
-            cell.dataset.col = `${j}`;
-
-            boardPlayer1.appendChild(cell);
-            if (board1[i][j] === 1) {
-                cell.classList.add("ship");
-            } else if (board1[i][j] === 2) {
-                cell.classList.add("damaged-ship");
-            } else if (board1[i][j] === 3) {
-                cell.classList.add("missed-attack");
-            }
-        }
-    }
-
-    const boardPlayer2 = document.createElement("div");
-    boardPlayer2.classList.add("board");
-    boardPlayer2.classList.add("ai-board");
-    boardContainer.appendChild(boardPlayer2);
-
-    for (let i = 0; i < board2.length; i++) {
-        for (let j = 0; j < board2[i].length; j++) {
-            const cell = document.createElement("div");
-            cell.classList.add("cell");
-            cell.dataset.row = `${i}`;
-            cell.dataset.col = `${j}`;
-
-            boardPlayer2.appendChild(cell);
-            if (board2[i][j] === 2) {
-                cell.classList.add("damaged-ship");
-            } else if (board2[i][j] === 3) {
-                cell.classList.add("missed-attack");
-            }
-        }
-    }
+    createBoard(board1, boardContainer, true);
+    createBoard(board2, boardContainer, false);
 }
+
 function showWinner(winner) {
     main.innerHTML = `<h1>${winner} wins!</h1>`;
+
     const restartButton = document.createElement("button");
     restartButton.classList.add("restart-button");
-    restartButton.innerHTML = "Restart";
+    restartButton.textContent = "Restart";
     main.appendChild(restartButton);
 }
 
 function lobby() {
     main.innerHTML = "";
+
     const startGameButton = document.createElement("button");
-    startGameButton.textContent = "Start Game";
     startGameButton.classList.add("start-game-button");
+    startGameButton.textContent = "Start Game";
     main.appendChild(startGameButton);
 }
+
 export { drawBoard, showWinner, lobby };
